f52ba6db48cf733ca327fbd6c485559a8424a7ad
Samoilov Daniil <dansamoru@ya.ru>
2021-02-27 20:30:07 +0300

    Add auth system

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(
+            self, username, email=None, password=None, **extra_fields
+    ):
+        if not username:
+            raise ValueError('The given username is empty')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=128)
+    email = models.EmailField(blank=True)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
--- api/messenger/settings.py
+++ api/messenger/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+
+    'authentication',
 ]
 
 REST_FRAMEWORK = {
