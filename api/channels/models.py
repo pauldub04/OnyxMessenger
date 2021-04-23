f33b61356aa56a191e49788cbe9f203d231fade9
Daniil Samoilov <dansamoru@ya.ru>
2021-04-23 23:34:47 +0300

    Add basic channel activity

--- api/channels/models.py
+++ api/channels/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ChannelMessage(models.Model):
+    text = models.TextField()
+    date = models.DateTimeField()
+    sender = models.IntegerField()
+    image = models.ImageField()
+    views_count = models.IntegerField(default=0)
+
+
+class Channel(models.Model):
+    name = models.CharField(63)
+    description = models.TextField()
+    admin = models.IntegerField()
+    messages = models.ManyToManyField(ChannelMessage)
--- api/channels/views.py
+++ api/channels/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import *
+
+
+def get_channel_messages(request):
+    messages = list(ChannelMessage.objects.filter(request.GET['channel_id']))
+    response = {
+        'response': [
+            {
+                'text': i.text,
+                'date': i.date,
+                'sender': i.sender,
+                'image': i.image,
+                'views_count': i.views_count,
+            }
+            for i in messages
+        ]
+    }
+    return JsonResponse(response)
