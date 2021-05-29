972939ade9ba4d0f93a1087453a3ac2d24f1d2e4
pauldub04 <paul.dubnyakov@gmail.com>
2021-05-29 12:51:35 +0300

    simple uploading image to backend

--- web_frontend/components/NavigationDrawer.vue
+++ web_frontend/components/NavigationDrawer.vue
@@ -59,6 +59,15 @@
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
+      <v-list-item :to="localePath('/images')">
+        <v-list-item-icon>
+          <v-icon> mdi-image </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title> images </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
 
     <v-spacer></v-spacer>
---
+++ web_frontend/pages/images.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <h3>Upload image</h3>
+    <v-file-input v-model="file"></v-file-input>
+    <v-btn @click="upload">Upload</v-btn>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    chatName: '',
+    file: null,
+  }),
+  methods: {
+    upload() {
+      console.log(this.file)
+      const formData = new FormData()
+      formData.append('image', this.file)
+      formData.append('title', 'dfg')
+      formData.append('type', 1)
+      formData.append('users', [])
+
+      this.$axios
+        .post('http://localhost:8000/api/chats/', formData)
+        .then((response) => {
+          console.log(response.data)
+          this.$router.push('/main')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
